86cd7dedccd9266b57deb1519af91b55d3f0917b
yingjiechen <YingjieChen@live.cn>
2015-09-09 10:28:21 +0800

    phpmail

--- thinkphp_3.2.3_full/Application/Home/Conf/config.php
+++ thinkphp_3.2.3_full/Application/Home/Conf/config.php
@@ -1,4 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	DEFAULT_CHARSET=>"utf-8",
+	
+	// 配置邮件发送服务器
+    'MAIL_HOST' =>'smtp.163.com',//smtp服务器的名称
+    'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
+    'MAIL_USERNAME' =>'15521109628@163.com',//你的邮箱名
+    'MAIL_FROM' =>'15521109628@163.com',//发件人地址
+    'MAIL_FROMNAME'=>'陈英捷',//发件人姓名
+	
+    'MAIL_PASSWORD' =>'jjj123456',//邮箱密码
+    'MAIL_CHARSET' =>'utf-8',//设置邮件编码
+    'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
 );
--- thinkphp_3.2.3_full/Application/Home/Controller/IndexController.class.php
+++ thinkphp_3.2.3_full/Application/Home/Controller/IndexController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Libiary\Vendor\Mail;
 class IndexController extends Controller {
     public function index(){
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover,{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
@@ -8,6 +9,16 @@ class IndexController extends Controller {
 	
 	public function index2()
 	{
-		$this->show("Hello world!");
+		$Ip = new \Org\Net\IpLocation('UTFWry.dat'); // 实例化类 参数表示IP地址库文件
+		$area = $Ip->getlocation('2.1.1.1'); // 获取某个IP地址所在的位置
+		$this->show('<div type="text/css">'.$area[country].'-->'.$area[area].'</div>','utf-8');
+	}
+	
+	public function index3()
+	{  
+        if(SendMail("15521109628@163.com","测试邮件。","测试内容。"))
+            $this->success('发送成功！');
+        else
+            $this->error('发送失败');
 	}
 }
